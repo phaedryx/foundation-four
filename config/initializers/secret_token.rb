7a1cb185fb5112ea20088eeca50fd23f2be7de92
Tad Thorley <phaedryx@gmail.com>
2013-03-04 15:40:28 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoundationFour::Application.config.secret_token = '47dc3d440f7900474568eee39187f13cf759736e957bfeff1ea5c1c3be22cad039093e71795a6d2d7ab844c68c44ff326edd20ba70583de1dcca2ae033e8199f'
